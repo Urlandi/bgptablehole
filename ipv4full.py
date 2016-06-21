251f001c76744d6472e75ea3201598be169d15a0
Urlandi <letitbee@yandex.ru>
2016-06-21 17:16:38 +0300

    Except for fileinput.py

--- ipv4full.py
+++ ipv4full.py
@@ -1,53 +1,59 @@
 import fileinput
 
-from ipv4holes import ipv4num, getholes, numipv4, issubnet, netsub
+from ipv4holes import getholes, numipv4, issubnet, netsub
 from prefixin import *
 
 ipstack = []
 prefix_spec_i = 0
 
-for line in fileinput.input():
+try:
+    for line in fileinput.input():
 
-    rline = line.rstrip()
+        rline = line.rstrip()
 
-    prefix = ipv4num(rline)
+        prefix = ipv4num(rline)
 
-    if not prefix:
-        print "Invalid prefix '{}'".format(rline)
-        exit(1)
+        if not prefix:
+            print "Invalid prefix '{}'".format(rline)
+            exit(1)
 
-    if prefix[1] > PREFIX_MAX:
-        continue
+        if prefix[1] > PREFIX_MAX:
+            continue
 
-    prefixes = [prefix]
+        prefixes = [prefix]
 
-    prefix_spec = PREFIX_SPEC[prefix_spec_i]
-
-    while prefix > prefix_spec and not issubnet(prefix_spec, prefix):
-        prefixes.insert(-1, prefix_spec)
-        prefix_spec_i += 1
         prefix_spec = PREFIX_SPEC[prefix_spec_i]
 
-    if issubnet(prefix, prefix_spec):
-        prefixes[-1:] = netsub(prefix, prefix_spec)
-    elif issubnet(prefix_spec, prefix):
-        prefixes[-1:] = [prefix_spec]
+        while prefix > prefix_spec and not issubnet(prefix_spec, prefix):
+            prefixes.insert(-1, prefix_spec)
+            prefix_spec_i += 1
+            prefix_spec = PREFIX_SPEC[prefix_spec_i]
 
-    for cur_prefix in prefixes:
+        if issubnet(prefix, prefix_spec):
+            prefixes[-1:] = netsub(prefix, prefix_spec)
+        elif issubnet(prefix_spec, prefix):
+            prefixes[-1:] = [prefix_spec]
 
-        holes, netunion, ipstack[:] = getholes(cur_prefix, ipstack)
+        for cur_prefix in prefixes:
 
-        for net in netunion:
-            sids = '+'
-            if net in PREFIX_SPEC:
-                sids = '*'
-            print ("{}{}/{}".format(sids, numipv4(net[0]), net[1]))
+            holes, netunion, ipstack[:] = getholes(cur_prefix, ipstack)
 
-        for hole in holes:
-            sids = '-'
-            if hole in PREFIX_SPEC:
-                sids = '*'
-            print ("{}{}/{}".format(sids,numipv4(hole[0]), hole[1]))
+            for net in netunion:
+                sids = '+'
+                if net in PREFIX_SPEC:
+                    sids = '*'
+                print ("{}{}/{}".format(sids, numipv4(net[0]), net[1]))
+
+            for hole in holes:
+                sids = '-'
+                if hole in PREFIX_SPEC:
+                    sids = '*'
+                print ("{}{}/{}".format(sids,numipv4(hole[0]), hole[1]))
+
+except IOError, ValueError:
+    print ("Input read error.")
+finally:
+    fileinput.close()
 
 for net in ipstack:
     sids = '+'
