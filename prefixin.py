091f9b8f4367dd59fda34f6787b81ae1a2a73d5f
Mikhail Vasilyev <letitbee@yandex.ru>
2018-03-26 16:07:48 +0300

    Maximum prefix length has been expanded to /32

--- prefixin.py
+++ prefixin.py
@@ -5,7 +5,7 @@ Work with special prefixes
 
 from ipv4seq import ipv4num, issubnet, netsub
 
-PREFIX_MAX = 24
+PREFIX_MAX = 32
 
 # http://www.iana.org./assignments/iana-ipv4-special-registry/iana-ipv4-special-registry.xhtml
 PREFIX_SPEC = (ipv4num("0.0.0.0/8"),
